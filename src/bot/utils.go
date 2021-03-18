bb88378939fb45de3c0ce0249611b451fb3d7707
Chetan Kini <ckini123@gmail.com>
2021-03-18 17:45:25 -0400

    refactor(utils): Move cron helpers to utils

--- src/bot/utils.go
+++ src/bot/utils.go
@@ -1,8 +1,13 @@
 package bot
 
 import (
+	"context"
+	"log"
 	"math/rand"
 	"time"
+
+	"cloud.google.com/go/firestore"
+	"google.golang.org/api/iterator"
 )
 
 func contains(list []string, cmd string) bool {
@@ -36,4 +41,70 @@ func shuffle(slice []Recurser) {
 		ret[i] = slice[randIndex]
 	}
 	slice = ret
+}
+
+func selectQuestion(recurser Recurser, client *firestore.Client, ctx context.Context) map[string]interface{} {
+	config := recurser.Config
+
+	if config.ManualQuestion {
+		return nil
+	}
+
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
+	difficulty := config.SoloDifficulty[r.Intn(len(config.SoloDifficulty))]
+	query := client.Collection("questions").Where("difficulty", "==", difficulty)
+
+	if config.Topics != nil && len(config.Topics) != 0 {
+		topic := config.Topics[r.Intn(len(config.Topics))]
+		query = query.Where("tags", "array-contains", topic)
+	}
+
+	if config.ProblemSet != "random" {
+		query = query.Where("psets", "array-contains", config.ProblemSet)
+	}
+
+	iter := query.Documents(ctx)
+	var documents []*firestore.DocumentSnapshot
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Panic(err)
+		}
+		documents = append(documents, doc)
+	}
+
+	if len(documents) == 0 {
+		return nil
+	}
+
+	selection := documents[r.Intn(len(documents))]
+
+	return selection.Data()
+}
+
+func iterToRecurserList(iter *firestore.DocumentIterator) []Recurser {
+	var recursersList []Recurser
+
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Panic(err)
+		}
+
+		var recurser Recurser
+		if err = doc.DataTo(&recurser); err != nil {
+			log.Fatal(err)
+		}
+		recursersList = append(recursersList, recurser)
+	}
+
+	return recursersList
 }
