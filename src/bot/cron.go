cf8e3da2950919e4a08dc42e37f5f5b4afd59eb1
Chetan Kini <ckini123@gmail.com>
2021-03-19 13:49:54 -0400

    chore(cron): Add AppEngine header for security

--- src/bot/cron.go
+++ src/bot/cron.go
@@ -14,10 +14,10 @@ import (
 func Cron(w http.ResponseWriter, r *http.Request) {
 	// Check that the request is originating from within app engine
 	// https://cloud.google.com/appengine/docs/flexible/go/scheduling-jobs-with-cron-yaml#validating_cron_requests
-	// if r.Header.Get("X-Appengine-Cron") != "true" {
-	// 	http.NotFound(w, r)
-	// 	return
-	// }
+	if r.Header.Get("X-Appengine-Cron") != "true" {
+		http.NotFound(w, r)
+		return
+	}
 
 	// setting up database connection
 	ctx := context.Background()
@@ -37,9 +37,6 @@ func Cron(w http.ResponseWriter, r *http.Request) {
 	case 12:
 		PostDaily(client, ctx)
 	default:
-		// MessageSolo(client, ctx)
-		// MessagePairs(client, ctx)
-		PostDaily(client, ctx)
-		// log.Fatal("Something is up with cron; this shouldn't be running! Check out your YAML")
+		log.Fatal("Something is up with cron; this shouldn't be running! Check out your YAML")
 	}
 }
